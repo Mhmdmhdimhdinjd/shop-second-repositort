3e156952e22094d666ac9a51f6c45eca3ef38115
mhmd mhdi <mhdimhdin5@gmail.com>
2025-01-06 13:48:21 +0330

    add navbar

---
+++ src/Apptest.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router , Route , Routes} from 'react-router-dom';
+
+
+const Apptest = () => {
+
+
+  return(
+
+<Router>
+
+    <Navbar/>
+
+<Routes>
+
+
+
+</Routes>
+
+</Router>
+
+  )
+}
+
+
+export default Apptest
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link , useLocation} from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import logo from '/src/assets/images/برنج نی نی 04.png'
+import './Navbar.css'
+
+const Navbar = () => {
+
+  const location = useLocation()
+
+  return (
+    <AppBar position="static" color="white">
+      <Toolbar>
+        <Box display="flex" flexGrow={1} alignItems="center" gap={2}>
+          <Link to="/ninishop2/Shoppingbasket">
+            <IconButton color="inherit">
+              <ShoppingBasketOutlinedIcon style={{color:'black'}}/>
+            </IconButton>
+          </Link>
+          <Link to="/ninishop2/Profile">
+            <IconButton color="inherit" onClick={() => localStorage.setItem('userredirect', location.pathname)}>
+              <AccountCircleOutlinedIcon style={{color:'black'}}/>
+            </IconButton>
+          </Link>
+          <Link to="/ninishop2/Products">
+            <Typography variant="body1" color="textPrimary" fontFamily={gandom}>
+              محصولات
+            </Typography>
+          </Link>
+        </Box>
+        <Box>
+          <Link to="/ninishop2/home">
+            <img src={logo} alt="logo" style={{ width: 64 }} />
+          </Link>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
+
