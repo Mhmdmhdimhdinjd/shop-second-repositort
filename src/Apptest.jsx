ec8a4cc72f8a6265dad378a03a1a731362bc0616
mhmd mhdi <mhdimhdin5@gmail.com>
2025-01-11 10:40:41 +0330

    change profile folder name

--- src/Apptest.jsx
+++ src/Apptest.jsx
@@ -5,9 +5,9 @@ import './styles/style.css'
 import Home from "./pages/Home/Home";
 import store from "./redux/store";
 import PrivateRoute from "./navigation/PrivateRoute";
-// import Profile from "/src/pages/profile/profile";
 import Products from "./pages/Products/products";
 import Shoppingbasket from "./pages/Shoppingbasket/ShoppingBasket";
+import Profile from "./pages/Profile/profile";
 
 
 const Apptest = () => {
@@ -32,7 +32,7 @@ const Apptest = () => {
 
                         <Route path='/shop/Home' element={<Home />} />
 
-                        {/* <Route path="/shop/profile" element={<Profile/>}  /> */}
+                        <Route path="/shop/profile" element={<Profile/>}  />
 
                         <Route path="/shop/products" element={<PrivateRoute><Products/></PrivateRoute>}/>
 
--- src/navigation/PrivateRoute.jsx
+++ src/navigation/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React , {useEffect}from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import Profile from '../pages/profile/profile';
+import Profile from '../pages/Profile/profile';
 
 
 const PrivateRoute = ({ children }) => {
